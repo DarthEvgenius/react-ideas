ba7896bce0383c3840d0af9b677e6c9deee05ab9
Eugene Zinin <e.zinin@joki-joya.ru>
2025-12-09 14:41:33 +0300

    generate index plugin added

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,11 +1,20 @@
 import { trpc } from '../lib/trpc'
 
-import { getIdeaTrpcRoute } from '../router/getIdea'
-import { getIdeasTrpcRoute } from '../router/getIdeas'
+/* Here we generate importing files via 'generate index' VS code extension
+ * use command pallete: cmd + shift + p --> generate index
+ * expect to have index.ts files in folders, and folder's names are used to generate names of functions for export
+ */
+
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
+import { getIdeaTrpcRoute } from './getIdea'
+import { getIdeasTrpcRoute } from './getIdeas'
+// @endindex
 
 export const trpcRouter = trpc.router({
-  getIdeas: getIdeasTrpcRoute,
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getIdea: getIdeaTrpcRoute,
+  getIdeas: getIdeasTrpcRoute,
+  // @endindex
 })
 
 export type TrpcRouterType = typeof trpcRouter
